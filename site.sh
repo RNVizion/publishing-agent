56348adbc786a0fc18cb2005e2aa29a4047cca5e
Christian Smith <18348535+RNVizion@users.noreply.github.com>
2026-07-15 09:37:17 -0400

    Update site.sh

--- site.sh
+++ site.sh
@@ -1,1 +1,3 @@
-cd /workspaces && git clone https://github.com/RNVizion/rnvizion.github.io.git
+cd /workspaces
+git clone https://github.com/RNVizion/rnvizion.github.io.git
+git clone https://github.com/RNVizion/ask-the-corpus.git
